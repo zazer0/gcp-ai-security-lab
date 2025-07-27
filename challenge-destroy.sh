925c7d70b8d6e358df5b0657e0f6569a4d887b0c
zazer0@ <zazer.unsw@gmail.com>
2025-07-27 23:56:01 +0000

    fix(chal-destroy): rename proj- -> _ alos

--- challenge-destroy.sh
+++ challenge-destroy.sh
@@ -6,12 +6,12 @@ PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID | grep projectNumber | tr 
 
 # destroy resources with terraform
 cd terraform
-terraform destroy -var project-id="$PROJECT_ID" -var project-number="$PROJECT_NUMBER" -auto-approve
+terraform destroy -var project_id="$PROJECT_ID" -var project_number="$PROJECT_NUMBER" -auto-approve
 cd ../
 
 # destroy resources for module 2 with terraform
 cd terraform_module2
-terraform destroy -var project-id="$PROJECT_ID" -var project-number="$PROJECT_NUMBER" -auto-approve
+terraform destroy -var project_id="$PROJECT_ID" -var project_number="$PROJECT_NUMBER" -auto-approve
 cd ../
 
 # clean up credential files we created
