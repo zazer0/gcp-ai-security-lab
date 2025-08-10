53c08a7ec26058c6b4090f1df4e3d35d3534cd5a
zazer0@ <zazer.unsw@gmail.com>
2025-08-10 17:45:17 +0000

    feat(configure_gcp): add missing cmds!

--- configure_gcp_project.sh
+++ configure_gcp_project.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-
-
 PROJECT_ID=${TF_VAR_project_id}
-echo "Parsed ${PROJECT_ID} from env!"
+echo "Parsed ${PROJECT_ID} from env! Running setup commands..."
 
 sleep 2
 
+gcloud compute disks list || gcloud auth login
 gcloud config set project "$PROJECT_ID"
+gcloud auth application-default login
 gcloud auth application-default set-quota-project "$PROJECT_ID"
