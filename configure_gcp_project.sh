dcb67a6caca25fb2096c6d2ea5935408ea68261a
zazer0@ <zazer.unsw@gmail.com>
2025-08-14 19:54:44 +0000

    fix(configGCP): silence intentional err

--- configure_gcp_project.sh
+++ configure_gcp_project.sh
@@ -5,7 +5,7 @@ echo "Parsed ${PROJECT_ID} from env! Running setup commands..."
 
 sleep 2
 
-gcloud compute disks list || gcloud auth login
+gcloud compute disks list 2>/dev/null || gcloud auth login
 gcloud config set project "$PROJECT_ID"
 gcloud auth application-default login
 gcloud auth application-default set-quota-project "$PROJECT_ID"
