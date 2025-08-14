e68472d1285d81ef17bcec413cf60f17e3e54a07
zazer0@ <zazer.unsw@gmail.com>
2025-08-14 15:14:15 +0000

    feat(configure_gcp): echo "run challenge-setup.sh!"

--- configure_gcp_project.sh
+++ configure_gcp_project.sh
@@ -9,3 +9,11 @@ gcloud compute disks list || gcloud auth login
 gcloud config set project "$PROJECT_ID"
 gcloud auth application-default login
 gcloud auth application-default set-quota-project "$PROJECT_ID"
+
+echo "Enabling required Gcloud API's..."
+bash steps_to_enable.sh || exit 1
+echo "Project API's enabled! "
+
+STARTYELLOW="\033[1;33m"
+ENDYELLOW="\033[0m"
+echo -e "${STARTYELLOW} Nice job! Now, run 'bash challenge-setup.sh' to deploy!${ENDYELLOW}
